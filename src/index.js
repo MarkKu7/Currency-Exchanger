a469bed52a26f2d3292b2d4c2e2297c7ff0392e7
MarkKu7 <marksrc7@gmail.com>
2021-09-10 11:54:11 -0700

    added code in index.js and html, non working WIP

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import getCurrency from './src/js/currency.js';
 
+$(document).ready(function() {
+  $('#exchange').click(function() {
 
+    const USD = $('#dollars').val();
+    const currency = $('#currency :selected').val();
+
+    let promise = getCurrency.getExchange();
+    promise.then(function(response) {
+      const data = JSON.parse(response); 
+      const rate = data.conversion_rates[currency];
+      const num = rate * USD;
+      const amount = num.toFixed(2);
+      $('.show-exchange').text(`The currency exchange rate is $${rate} and the amount is ¤${amount}`);
+    });
+  });
+})
